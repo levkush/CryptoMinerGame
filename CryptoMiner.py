d35c8f67facc83b90f8b87bf4adbe4a631b5f7c3
Lev Kushtym <lev.kushtym@gmail.com>
2022-09-08 15:47:47 +0200

    Add saves to the game

--- CryptoMiner.py
+++ CryptoMiner.py
@@ -1,3 +1,4 @@
+# Imports
 import os
 import sys
 import pyfiglet
@@ -5,17 +6,75 @@ from colorama import Fore
 import random
 import threading
 import time
+import json
+import base64
 
-global money
-global multiplier
-global upgradeprice
 
-money = 0
-multiplier = 1
-upgradeprice = 25
+
+
+# Saving
+def Save(money, multiplier, upgradeprice):
+    print(money)
+    if not os.path.exists(os.environ['HOME'] + "/.CryptoMiner"):
+        os.mkdir(os.environ['HOME'] + "/.CryptoMiner")
+        
+    os.chdir(os.environ['HOME'] + "/.CryptoMiner")
+    
+    # Data to be written
+    dictionary = {
+        "money" : money,
+        "upgradeprice" : upgradeprice,
+        "multiplier" : multiplier,
+    }
+
+    # Serializing json 
+    json_object = json.dumps(dictionary, indent = 4)
+    
+    # Writing
+    with open("save.json", "w", encoding="utf-8") as outfile:
+        outfile.write(json_object)
+        print("Saved!")
+
+# Loading
+def Load():
+    os.chdir(os.environ['HOME'] + "/.CryptoMiner")    
+    # Opening JSON file
+    with open('save.json', 'r') as openfile:
+        # Reading from json file
+        save = json.load(openfile)
+
+    global money
+    global multiplier
+    global upgradeprice
+    
+    money = save["money"]
+    upgradeprice = save["upgradeprice"]
+    multiplier = save["multiplier"]
+
+# Check for saves
+def SaveCheck():
+    if os.path.exists(os.environ['HOME'] + "/.CryptoMiner"):
+        Load()
+        print("Progress backuped!")
+        return None
+    
+    # If no saves defaulting
+    global money
+    global multiplier
+    global upgradeprice
+    
+    money = 0
+    multiplier = 1
+    upgradeprice = 25
+    
 btcprice = 10000
 
-os.system("cls")
+
+# Check the system type and clear console
+if os.name == "nt":
+    os.system("cls")
+else:
+    os.system("clear")
 
 fred = Fore.RED
 fyellow = Fore.YELLOW
@@ -29,11 +88,12 @@ fblack = Fore.BLACK
 print(fgreen, pyfiglet.figlet_format("CryptoFarmer"))
 
 def main(): 
+    SaveCheck()
+    
     btcwatchdog = threading.Thread(target=bitcoinprice, daemon=True)
     btcwatchdog.start()
     
     CommandInterpreter()
-
 
 def bitcoinprice():
     while True:
@@ -42,6 +102,10 @@ def bitcoinprice():
         global multiplier
         global btcprice
         global localprice
+        global money
+        global upgradeprice
+        
+        Save(money, multiplier, upgradeprice)
         
         percentage = random.randint(10, 20)
         percentage = (percentage / 100)
@@ -65,7 +129,7 @@ def bitcoinprice():
             btcprice = round(btcprice - (percentage * btcprice))
             multiplier = round(multiplier - (percentage * multiplier), 1)
             
-            print(f"\n\n{fred}:( {fyellow}Биткойн {fwhite}упал в цене. Новая цена: {fgreen}{btcprice}${fwhite}.\n")
+            print(f"\n\n{fred}:( {fyellow}Биткойн {fwhite}упал в цене. Новая цена: {fgreen}{btcprice}${fwhite}.\n")       
         
 
 
@@ -93,14 +157,14 @@ def upgrade():
     
 def balance():
     global money
-    retmoney = str(money) + " USDT"
+    money = round(money, 2)
+    retmoney = str(money) + "$"
     return retmoney
 
 def CommandList():
     len_now = 0
     CommandList = ["help", "m", "upgrade", "money", "btc"]
     DescriptionList = ["- Вывести это сообщение.", "- Добывает биткойн и увеличивает ваш баланс.", "- Обновляет ваши видеокарты. Дает прирост к заработку.", "- Выводит количиство ваших денег.", "- Курс биткойна сейчас"]
-    
     while len_now < len(DescriptionList) and len_now < len(CommandList):
         print(fyellow + CommandList[len_now] + fwhite + ' ' + DescriptionList[len_now])
         len_now = len_now + 1
